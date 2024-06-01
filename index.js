1488567ddb7980cb03f4a965e295490478f9da91
Georgina Riebelle <135925781+Geobelle@users.noreply.github.com>
2024-06-01 21:43:20 +0100

    adjusted javascript

--- index.js
+++ index.js
@@ -3,27 +3,28 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-let memf = document.getElementById("memf")
-let litg = document.getElementById("litg")
-let kilp = document.getElementById("kilp")
+const memf = document.getElementById("memf")
+const litg = document.getElementById("litg")
+const kilp = document.getElementById("kilp")
 const covertBtn = document.getElementById("convert")
 let input = document.getElementById("myInput").value = ""
 
+function meters(number){
+    let meter = number * 3.281
+    let feet = number / 3.281
+    memf.textContent = `${number} meters = ${meter.toFixed(3)} feet | ${number} feet = ${feet.toFixed(3)} meters`
+
+    let liter = number * 0.264
+    let gallon = number / 0.264
+    litg.textContent = `${number} liters = ${liter.toFixed(3)} gallons | ${number} gallons = ${gallon.toFixed(3)} liters`
+
+    let kilo = number * 2.204
+    let pound = number / 2.204
+    kilp.textContent = `${number} pounds = ${kilo.toFixed(3)} pounds | ${number} pounds = ${pound.toFixed(3)} kilos`
+
+}
 covertBtn.addEventListener("click", function() {
-        let input = document.getElementById("myInput").value
+        let input = document.getElementById("myInput").value 
         
-        let meter = input * 3.281
-        let feet = input / 3.281
-        memf.textContent = `${input} meters = ${meter.toFixed(3)} feet | ${input} feet = ${feet.toFixed(3)} meters`
-
-        let liter = input * 0.264
-        let gallon = input / 0.264
-        litg.textContent = `${input} liters = ${liter.toFixed(3)} gallons | ${input} gallons = ${gallon.toFixed(3)} liters`
-
-        let kilo = input * 2.204
-        let pound = input / 2.204
-        kilp.textContent = `${input} pounds = ${kilo.toFixed(3)} pounds | ${input} pounds = ${pound.toFixed(3)} kilos`
-
-    }
-)
-
+        meters(input)
+    })
